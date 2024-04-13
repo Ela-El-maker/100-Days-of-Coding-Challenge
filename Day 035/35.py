1a7fac085e6f5ce4b84ca22216aa5c7398d135cc
Ela-El-maker <feloela101@gmail.com>
2024-04-14 02:15:30 +0300

    Day 0350

--- Day 035/35.py
+++ Day 035/35.py
@@ -1,13 +1,19 @@
-	count = 0
-    for c in word:
-        if c.isupper() is True:
-            count += 1
-            
-    L = len(word)
-    if count == L:
-        return True
-    elif count == 0:
-        return True
-    elif count == 1 & word[0].isupper():
-        return True
-    else: return False
+class TwoSum:  
+    def __init__(self, list1, target):  
+        self.list1 = list1  
+        self.target = target  
+          
+    def solution(self):  
+        length = len(list1)  
+          
+        for i in range(length-1):  
+            for j in range(i+1, length):  
+                if list1[i]+list1[j] == self.target:  
+                    new_list = i, j  
+                    return list(new_list)  
+        return -1  
+  
+list1 = [1, 2, 4, 5, 11]  
+target = 6  
+obj = TwoSum(list1, target)  
+print(obj.solution()) 
