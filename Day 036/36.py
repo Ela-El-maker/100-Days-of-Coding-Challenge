1708196d3bcc1e32113841e8abc3f61889751924
Ela-El-maker <feloela101@gmail.com>
2024-04-15 02:21:17 +0300

    Using TRUE or FALSE

--- Day 036/36.py
+++ Day 036/36.py
@@ -3,12 +3,18 @@ def compare_string_lengths(str1, str2):
     This function takes two strings and returns True if the first string is longer than the second one, otherwise False.
     """
     # Your code here
-    if len(str1) == len(str2):
-        print(f"{str1} and {str2} are both equal in length")
-    elif len(str1) < len(str2):
-        print(f"{str2} is longer than {str1}.")
+    # if len(str1) == len(str2):
+    #     print(f"{str1} and {str2} are both equal in length")
+    # elif len(str1) < len(str2):
+    #     print(f"{str2} is longer than {str1}.")
+    # else:
+    #     print(f"{str1} is longer than {str2}")
+
+
+    if len(str1) > len(str2):
+        return True
     else:
-        print(f"{str1} is longer than {str2}")
+        return False
 
 
-print(compare_string_lengths('Hello','How are you'))
+print(compare_string_lengths('One','ee'))
