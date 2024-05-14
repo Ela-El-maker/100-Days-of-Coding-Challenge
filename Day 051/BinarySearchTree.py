08136cd12f1e5d130939965bcd033da8bef0ad80
Ela-El-maker <119635986+Ela-El-maker@users.noreply.github.com>
2024-05-14 18:47:29 +0000

    BinarySearchTree

--- Day 051/BinarySearchTree.py
+++ Day 051/BinarySearchTree.py
@@ -25,5 +25,40 @@ class BinarySearchTree(object):
       self.inOrderPlace(root,rchild)
 
 
-  
+  def preOrderPlace(self,root):
+    if not root:
+      return None
+    else:
+      print(root.val)
+      self.preOrderPlace(root,lchild)
+      self.preOrderPlace(root,rchild)
+
+  def postOrderPlace(self,root):
+    if not root:
+      return None
+    else:
+      self.postOrderPlace(root,lchild)
+      self.postOrderPlace(root,rchild)
+      print(root.val)
+
+
+
+"""Create different node and insert data into it"""
+
+r = Node(3)
+node = BinarySearchTree()
+nodeList = [1,22,3,44,5,66,7,88,9]
+
+for nd in nodeList:
+  node.insert(r,Node(nd))
+
+
+
+print("-------In order------")
+print(node.inOrderPlace(r))
+print("-------Pre order------")
+print(node.preOrderPlace(r))
+print("-------Post order------")
+print(node.postOrderPlace(r))
+
     
