2dc89ac22e8bc8e5629c00c46718d9f7a8e3d0cc
Ela-El-maker <119635986+Ela-El-maker@users.noreply.github.com>
2024-05-16 10:07:49 +0000

    Describing the algorithm

--- Day 054/binarySearch.py
+++ Day 054/binarySearch.py
@@ -53,3 +53,9 @@ def binarySearch(input, searchValue):
 value = 56
 array = [11, 22, 10, 32, 34, 56, 31, 89]
 print(binarySearch(array, value))
+
+# Time Complexities : Best => O(1), Average => O(log n) , Worst => O(log n)
+# Space Complexities : O(1)
+# Binary Search compares the target value to the middle elememnt of the array(has to be sorted).
+# If they are not equal, the search continues on the half in which the target is expected to be, again taking the middle element to compare to the target, and repeating this until the target is found.
+# If the search ends with the remaining half being empty, the target is not in the array
