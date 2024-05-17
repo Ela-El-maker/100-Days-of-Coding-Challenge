09df850ee1e5cc94b437ffa2f3d3efbb847bc6ba
Ela-El-maker <119635986+Ela-El-maker@users.noreply.github.com>
2024-05-17 18:34:49 +0000

    Day 055 : Linear Search Algorithm

---
+++ Day 055/linearSearch.py
@@ -0,0 +1,14 @@
+### Linear Search Algorithm
+
+# linearSearch(input, searchValue){
+#   from typing import TypeVarTuple
+
+
+# for(i = 0 to input.length -1){
+#     if(input[i] == searchValue)
+#       return True
+#   }
+#   return False
+# }
+
+
