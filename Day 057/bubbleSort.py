ca847b04b3ba5f47e2aa68b7298f8b98dbc7efc5
Ela-El-maker <119635986+Ela-El-maker@users.noreply.github.com>
2024-05-18 19:31:05 +0000

    BubbleSort algorithm in python

--- Day 057/bubbleSort.py
+++ Day 057/bubbleSort.py
@@ -15,3 +15,21 @@
 # end for 
 # return array 
 # end bubbleSort
+
+def bubbleSort(array):
+  n = len(array)
+  for i in range(n):
+    swapped = False
+    for j in range(0, n-i-1):
+      if array[j] > array[j+1]:
+        array[j], array[j+1] = array[j+1], array[j]
+        swapped = True
+      if swapped == False:
+        break
+
+if __name__ == "__main__":
+  array = [64,12,34,54,33,66,87,54]
+  bubbleSort(array)
+  print("Sorted array: ")
+  for i in range(len(array)):
+    print("%d" % array[i], end = "")
