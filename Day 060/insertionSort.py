0d5e0443dff69a571a1a770c0affb98b57fdea2c
Ela-El-maker <119635986+Ela-El-maker@users.noreply.github.com>
2024-05-21 13:01:01 +0000

    Insertion Sort algorithm in example

--- Day 060/insertionSort.py
+++ Day 060/insertionSort.py
@@ -22,5 +22,35 @@
 # }
 ### insertion sort algorithm
 
-def insertionSort(array):
+# def insertionSort(array):
+#   for i in range(len(array)):
+#     temp = array[i]
+#     hasMoved = False
+#     notPlaced = True 
+#     j = i - 1
+
+#     while j>= 0 and temp < array[j]:
+#       array[j+1] = array[j]
+#       hasMoved = True
+#       j -= 1
+
+#     array[j+1] = temp 
+
+# array = [90,2,3,32,57,2122,767,43323,799]
+# print(insertionSort(array))
+
+
+def insertionSort(arr):
+  for i in range(1, len(arr)):
+      key = arr[i]
+      j = i - 1
+      while j >= 0 and key < arr[j]:
+          arr[j + 1] = arr[j]
+          j -= 1
+      arr[j + 1] = key
+
+# Test the insertionSort function
+arr = [12, 11, 13, 5, 6]
+insertionSort(arr)
+print("Sorted array:", arr)
   
