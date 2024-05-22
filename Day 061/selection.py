5b3fe6c2015eac0f61af352c13d04dccf406dd00
Ela-El-maker <119635986+Ela-El-maker@users.noreply.github.com>
2024-05-22 17:00:33 +0000

    Selection Algorithm

---
+++ Day 061/selection.py
@@ -0,0 +1,18 @@
+## Selection Sort Algorithm
+
+
+# Adds sorted numbers to the right end of the array
+# for(i = input.length-1 to i > 0){
+#   int maxIndex = 0;
+#   for (j = i to j <= i){
+#     if(input[j] > input[maxIndex]){
+#       # Set index for largest element found till now
+#       maxIndex = j;
+      
+#     }
+#   }
+#   if(maxIndex != i){
+#     swap input[i] & input[maxIndex]
+#   }
+# }
+
